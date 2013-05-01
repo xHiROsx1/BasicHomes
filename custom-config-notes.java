032f32822a8909952c351f13dfc6f27b5eee3654
xHiROsx1 <kslrtips2@gmail.com>
2013-05-01 14:19:39 -0300

    Create custom-config-notes.java

---
+++ custom-config-notes.java
@@ -0,0 +1,17 @@
+package me.temp.test;
+
+//Ignore the missing imports.
+//Coded this on a we browser.
+
+public class Test extends JavaPlugin {
+  @Overrid
+  public void onEnable() {
+    //Enable
+  }
+  @Override
+  public void onDisable() {
+    //Disable
+  }
+////////////////////////////////////////////////
+//Custom Config: data.yml  -  HiROs15         //
+////////////////////////////////////////////////
